03ce74e876109065551432001dda254637185efc
Matthieu <matthieu.dabin@ynov.com>
2018-03-14 11:14:54 +0100

    fichier pour commenter
    voila

---
+++ commentaire.php
@@ -0,0 +1,10 @@
+<?php
+    print "\tcoucou\n";//retour à la ligne interprété
+    print '\tcoucou\n';//non interprété
+
+    /*
+    commentaire
+    sur plusieurs
+    Lignes*/
+    print 'C\'finit';
+    die("C'est finit vraiment");
