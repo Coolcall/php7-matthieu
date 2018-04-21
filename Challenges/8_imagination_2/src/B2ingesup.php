7810d858f44c91b5a54b04472ff0315893df19cc
Matthieu <matthieu.dabin@ynov.com>
2018-04-21 14:09:45 +0200

    mise à jour tous

--- Challenges/8_imagination_2/src/B2ingesup.php
+++ Challenges/8_imagination_2/src/B2ingesup.php
@@ -5,18 +5,10 @@
 class B2ingesup extends ecole
 {
 
-
-
     public function plusFort()
     {
         print "ils sont plus fort.";
     }
-
-
-
-
-
-
 
 }
 
--- testeval.php
+++ testeval.php
@@ -1,1 +1,12 @@
 <?php
+$secret = 'onnAoott0aaAhhbCCà';
+$next = '';
+
+for($i= strlen($secret)-1; $i >=0;$i--) {
+    if($next === $secret{$i}) {
+        print $secret{$i};
+    }
+    $next = $secret{$i};
+}
+
+print $secret{$i};
